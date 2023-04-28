2cbf2dc99d6555fc39ae4e8c72c4eb4c53c12bef
José Antonio Perdiguero López <perdy@perdy.io>
2023-04-28 13:57:31 +0200

    :tada: Work in Progress site

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://bosque.dev',
+  generateRobotsTxt: true,
+}
---
+++ src/next-seo.config.js
@@ -0,0 +1,18 @@
+const SEO = {
+  titleTemplate: '%s - Bosque',
+  defaultTitle: 'Bosque',
+  description: 'Store your models in this forest 🌳',
+  canonical: 'https://bosque.dev',
+  openGraph: {
+    type: 'website',
+    locale: 'en_EN',
+    url: 'https://bosque.dev',
+    title: 'Bosque',
+    description: 'Store your models in this forest 🌳',
+    site_name: 'bosque.dev',
+    imageWidth: 1280,
+    imageHeight: 640,
+  },
+}
+
+export default SEO
